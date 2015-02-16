fbe5fa3d05cbaa071d8cbd0a207c69847fc9175d
Vamsi Jandhayala <javamsi@seas.upenn.edu>
2015-02-16 02:22:55 -0500

    commit message

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150216064318) do
+ActiveRecord::Schema.define(version: 20150216065208) do
 
   create_table "statuses", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
